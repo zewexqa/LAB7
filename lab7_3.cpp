9802885bfb3bf712c057d39646b5b2c77fd5cff0
zewexqa <domtui555@gmail.com>
2024-12-03 16:46:06 +0700

    commit lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if (x >= 'A' && x <= 'Z'){
+        if (x == 'A'){
+            return 'Z';
+        } else {
+            return x - 1;
+        }
+    } else {
+        return '0';
+    }
 }
 
 int main(){
